8ab7dc5e13f46908763b6085728b2413e3398fe9
Jenny Mai <nhumai2@illinois.edu>
2024-05-13 15:56:37 +1000

    tune

--- agent/mcts.py
+++ agent/mcts.py
@@ -72,7 +72,7 @@ class MCTS:
         if not node.children:
             node.uct = heuristic_evaluation(node.board, node.mycolor)
         elif node.parent is not None:
-            node.uct = (node.win / node.visit) + (self.exploration_constant * math.sqrt(math.log(node.parent.visit) / node.visit)) + heuristic_evaluation(node.board, node.mycolor) * 0.5
+            node.uct = (node.win / node.visit) + (self.exploration_constant * math.sqrt(math.log(node.parent.visit) / node.visit)) + heuristic_evaluation(node.board, node.mycolor) * 0.4
             self.backpropagation(node.parent, won)
         else:
             node.uct = (node.win / node.visit)
